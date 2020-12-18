8d50271b7a3699c548828857bb0433dd96424533
nastiaa <lucius2305@gmail.com>
2020-12-18 17:11:36 +0200

    implemented parse for hex strings

--- lab2/Program.cs
+++ lab2/Program.cs
@@ -1,11 +1,51 @@
 ﻿using System;
 using System.Linq;
 using System.Numerics;
+using System.Globalization;
 
 namespace asymCrypto_2
 {
+  
     class Program
     {
+        static string HexToBin(string hexString)
+        {
+            string binString = "";
+            for (int i = 0; i < hexString.Length; i++)
+            {
+                if (hexString[i] == '0') { binString += "0000";}
+                if (hexString[i] == '1') { binString += "0001"; }
+                if (hexString[i] == '2') { binString += "0010"; }
+                if (hexString[i] == '3') { binString += "0011"; }
+                if (hexString[i] == '4') { binString += "0100"; }
+                if (hexString[i] == '5') { binString += "0101"; }
+                if (hexString[i] == '6') { binString += "0110"; }
+                if (hexString[i] == '7') { binString += "0111"; }
+                if (hexString[i] == '8') { binString += "1000"; }
+                if (hexString[i] == '9') { binString += "1001"; }
+                if (hexString[i] == 'A') { binString += "1010"; }
+                if (hexString[i] == 'B') { binString += "1011"; }
+                if (hexString[i] == 'C') { binString += "1100"; }
+                if (hexString[i] == 'D') { binString += "1101"; }
+                if (hexString[i] == 'E') { binString += "1110"; }
+                if (hexString[i] == 'F') { binString += "1111"; }
+            }
+
+            return binString;
+        }
+
+        static BigInteger ParseHex(string hexStr)
+        {
+            string binary = HexToBin(hexStr);
+            BigInteger res = 0;
+            foreach (char c in binary)
+            {
+                res <<= 1;
+                res += c == '1' ? 1 : 0;
+            }
+
+            return res;
+        }
         //-------------------------------------------------------- M I L L E R  -  R A B I N    T E S T ------------------------------------------------------------------------------------
         static int pq_Length = 32;
         static BigInteger TWO = new BigInteger(2);
@@ -191,124 +231,13 @@ namespace asymCrypto_2
             return Verify(k, S, n, e);
         }
 
+
         //---------------------------------------------------------------------------------------------------------------------------------------------------------------------
-        
+
         static void Main(string[] args)
         {
-            ////var nA = BigInteger.Parse("", System.Globalization.NumberStyles.AllowHexSpecifier);
-            ////var dA = BigInteger.Parse("", System.Globalization.NumberStyles.AllowHexSpecifier);
-            ////var eA = BigInteger.Parse("", System.Globalization.NumberStyles.AllowHexSpecifier);
-            ////var phi_nA = BigInteger.Parse("", System.Globalization.NumberStyles.AllowHexSpecifier);
-
-            ////var pA = BigInteger.Parse("7E2C7F4B871D2C2EAF587660F37CA6EB", System.Globalization.NumberStyles.AllowHexSpecifier);
-            ////var qA = BigInteger.Parse("7042D42A2EEDC650970099F6B144403A8F", System.Globalization.NumberStyles.AllowHexSpecifier);
-
-            ////var pB = BigInteger.Parse("495FBF6F01EAC752E9A3FF71FE51D747", System.Globalization.NumberStyles.AllowHexSpecifier);
-            ////var qB = BigInteger.Parse("1651AF4F6C011403258598985C57283599", System.Globalization.NumberStyles.AllowHexSpecifier);
-
-            ////if (pB * qB < pA * qA)
-            ////{
-            ////    var tempP = pA;
-            ////    var tempQ = qA;
-            ////    pA = pB;
-            ////    qA = qB;
-            ////    pB = tempP;
-            ////    qB = tempQ;
-            ////}
-
-            ////var nA = pA * qA;
-            ////var eA = new BigInteger(Math.Pow(2, 16) + 1);
-            ////BigInteger phi_nA = (pA - BigInteger.One) * (qA - BigInteger.One);
-            ////var dA = Inverse(eA, phi_nA);
-            var MA = BigInteger.Parse("66A0960A6704230455AB8013276AD3022C2DC5F0158B29EBCF45B229D8EBDEF0", System.Globalization.NumberStyles.AllowHexSpecifier);
-
-
-
-
-            ////var nB = pB * qB;
-            ////var eB = new BigInteger(Math.Pow(2, 16) + 1);
-            ////BigInteger phi_nB = (pB - BigInteger.One) * (qB - BigInteger.One);
-            ////var dB = Inverse(eB, phi_nB);
-
-            //var MB = BigInteger.Parse("BEA4BA4EC0BD4E8141E3E8DBB3AD5842378D8253E91544C96D994E2DBAB6EFE0", System.Globalization.NumberStyles.AllowHexSpecifier);
-
-
-
-            var keysA = GenerateKeyPair();
-            var keysB = GenerateKeyPair();
-
-            while (keysB.Item2.Item1 < keysA.Item2.Item1) // while (n1 < n) => generate new p,q,n...
-            {
-                keysA = GenerateKeyPair();
-                keysB = GenerateKeyPair();
-            }
-
-            //----------------------------------------------------- Encryption-Decryption for Abonent A   -----------------------------------------------------------------------
-            Console.WriteLine("\nABONENT A: \n");
-            BigInteger nA = keysA.Item2.Item1,
-                       eA = keysA.Item2.Item2,
-                       dA = keysA.Item1.Item1;
-
-            //BigInteger MA = new BigInteger(GenerateRandomByteSeed(62));
-            BigInteger CA = Encrypt(MA, nA, eA);
-            BigInteger MA_decrypted = Decrypt(CA, dA, nA);
-            BigInteger SA = Sign(MA, nA, dA);
-            bool verA = Verify(MA, SA, nA, eA);
-
-            Console.WriteLine("n = " + nA.ToString("X"));
-            Console.WriteLine("e = " + eA.ToString("X"));
-            Console.WriteLine("d = " + dA.ToString("X"));
-            Console.WriteLine("M = " + MA.ToString("X"));
-            Console.WriteLine("C = " + CA.ToString("X"));
-            Console.WriteLine("M_decrypted = " + MA_decrypted.ToString("X"));
-            Console.WriteLine("S = " + SA.ToString("X"));
-            Console.WriteLine(verA ? "Verified" : "Not verified");
-
-            ////----------------------------------------------------- Encryption-Decryption for Abonent B   -----------------------------------------------------------------------
-            //Console.WriteLine("\nABONENT B: \n");
-            //BigInteger nB = keysB.Item2.Item1,
-            //           eB = keysB.Item2.Item2,
-            //           dB = keysB.Item1.Item1;
-
-            ////BigInteger MB = new BigInteger(GenerateRandomByteSeed(62));
-            //BigInteger CB = Encrypt(MB, nB, eB);
-            //BigInteger MB_decrypted = Decrypt(CB, dB, nB);
-            //BigInteger SB = Sign(MB, nB, dB);
-            //bool verB = Verify(MB, SB, nB, eB);
-
-            //Console.WriteLine("n = " + nB.ToString("X"));
-            //Console.WriteLine("e = " + eB.ToString("X"));
-            //Console.WriteLine("d = " + dB.ToString("X"));
-            //Console.WriteLine("M = " + MB.ToString("X"));
-            //Console.WriteLine("C = " + CB.ToString("X"));
-            //Console.WriteLine("M_decrypted = " + MB_decrypted.ToString("X"));
-            //Console.WriteLine("S = " + SB.ToString("X"));
-            //Console.WriteLine(verB ? "Verified" : "Not verified");
-
-            ////---------------------------------------------------- SEND - RECIEVE KEY ----------------------------------------------------------------------------
-            ////var k = GenerateBigInteger(nA - BigInteger.One);
-            //var k = BigInteger.Parse("DC3591FE994A74FFD7662F90A08251CDA5A13959413AF5DCFBFF6D12CC59DC96", System.Globalization.NumberStyles.AllowHexSpecifier);
-
-            //Console.WriteLine("\n Send: \n");
-            //var k1S1 = SendKey(eB, nB, nA, dA, k);
-
-            //Console.WriteLine("k = " + k.ToString("X"));
-            //Console.WriteLine("k1 = " + k1S1.Item1.ToString("X"));
-            //Console.WriteLine("S1 = " + k1S1.Item2.ToString("X"));
-
-            //Console.WriteLine("\n Recieved: \n");
-            //var recieved = RecieveKey(dB, nB, k1S1.Item1, k1S1.Item2, eA, nA);
-            //Console.WriteLine(recieved);
-
-
-
-            var n = BigInteger.Parse("0A8BAE80602C9C257FF49E6D52E06FA28C09C305194C83771A83C27B31C0555591985FBF6BE73CCD7C01D01AE0446111D98AE0F9360CEE5C283573D3197B6D95", System.Globalization.NumberStyles.AllowHexSpecifier);
-            var e = BigInteger.Parse("10001", System.Globalization.NumberStyles.AllowHexSpecifier);
-            var d = BigInteger.Parse("79F92361B502450B77F0E47D9718541EC19E270FD28E2A066FA78CCB5DA63D57A61D431BF8F018C0116FD257C200666A97367923F3F8B53E11C3092A7BF3A01", System.Globalization.NumberStyles.AllowHexSpecifier);
-
-            var C = BigInteger.Parse("0100A1586A33B4C9D44F36FA6ABCCE162DBCC05ABF7C4184BC37C923A391F6A91B8ED9884220742E3B66B6108410352DC33339FFEE373C3532CFA81823C9EB81", System.Globalization.NumberStyles.AllowHexSpecifier);
-            var M = Decrypt(C, d, n);
-            Console.WriteLine(M.ToString("X"));
+            var m = ParseHex("BCCAE63B06355BCDA56EE08F4127DFF5E38C6CAE9B3BEB2CCD5887C5D6DD1E15");
+            Console.WriteLine(m.ToString("X"));
             Console.ReadKey();
         }
     }
