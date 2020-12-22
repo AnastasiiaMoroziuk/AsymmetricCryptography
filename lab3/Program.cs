e3f68e951fa581ef3e7a4d7459d6b0763e1ed979
nastiaa <lucius2305@gmail.com>
2020-12-22 03:49:11 +0200

    Lab 3 done exect decryption

--- lab3/Program.cs
+++ lab3/Program.cs
@@ -106,13 +106,16 @@ namespace Asym_Crypto_Lab_3
 
         static Tuple<BigInteger, BigInteger, BigInteger, BigInteger> BlumSqrt(BigInteger y, BigInteger p, BigInteger q)
         {
-            BigInteger s1 = BigInteger.ModPow(y, (p + 1) / 4, p);
-            BigInteger s2 = BigInteger.ModPow(y, (q + 1) / 4, q);
-            var temp = GCD_Ext(p, q);
-            BigInteger x1 = (temp.Item1 * p * s1) + (temp.Item2 * q * s2);
-            BigInteger x2 = (temp.Item1 * p * s1) - (temp.Item2 * q * s2);
-            BigInteger x3 = -(temp.Item1 * p * s1) + (temp.Item2 * q * s2);
-            BigInteger x4 = -(temp.Item1 * p * s1) - (temp.Item2 * q * s2);
+            BigInteger s1 = BigInteger.ModPow(y, (p + BigInteger.One) / FOUR, p);
+            BigInteger s2 = BigInteger.ModPow(y, (q + BigInteger.One) / FOUR, q);
+            var n = p * q;
+            //var temp = GCD_Ext(p, q);
+            BigInteger u = Inverse(p, q);
+            BigInteger v = Inverse(q, p);
+            BigInteger x1 = (((u * p * s2) % n) + ((v * q * s1) % n))%n;
+            BigInteger x2 = (((u * p * s2) % n) - ((v * q * s1) % n))%n;
+            BigInteger x3 =/* -((u * p * s2) % n) + ((v * q * s1) % n)*/ n - x1;
+            BigInteger x4 = /*-((u * p * s2) % n) - ((v * q * s1) % n)*/ n - x2;
             return Tuple.Create(x1, x2, x3, x4);
         }
 
@@ -154,17 +157,6 @@ namespace Asym_Crypto_Lab_3
             return res;
         }
 
-        static BigInteger Trim(BigInteger num, int desiredLength)
-        {
-            var bytes = num.ToByteArray();
-            var newArr = new byte[desiredLength];
-            if(bytes.Length > desiredLength)
-            {
-                Array.Copy(bytes, newArr, desiredLength);
-                return new BigInteger(newArr);
-            }
-            return num;
-        }
         /* Prime numbers generating */
         static BigInteger GenerateBigInteger(BigInteger max)
         {
@@ -304,16 +296,15 @@ namespace Asym_Crypto_Lab_3
         static BigInteger DeformatMessage(BigInteger x, BigInteger n)
         {
             var l = n.ToByteArray().Length;
-            BigInteger m = (x - r - new BigInteger(255 * Math.Pow(2, 8 * (l - 2)))) / new BigInteger(Math.Pow(2, 64));
-            return m;
+            var xStr = x.ToString("X");
+            var xWithoutR = xStr.Substring(0, xStr.Length - 16);
+            return ParseHex((xStr.Length == 128 ? xWithoutR.Substring(4) : xWithoutR.Substring(3)).TrimStart('0'));
         }
 
         static Tuple<BigInteger, BigInteger, BigInteger> Encrypt(BigInteger x, BigInteger b, BigInteger n)
         {
             var bHalf = (b*Inverse(2, n)) % n;
             var y = (x * (x + b))%n;
-            if (y < n) { Console.WriteLine("-------------------cokay ---------"); }
-            else { Console.WriteLine(y-n); }
             var c1 = ((x + bHalf) % n) % 2;
             var c2 = IversonSymbol(x + bHalf, n);
             //var c2 = JacobiSymbol(x+bHalf, n) == BigInteger.One ? BigInteger.One : BigInteger.Zero;
@@ -323,52 +314,252 @@ namespace Asym_Crypto_Lab_3
         //                                   y          c1           c2                           p            q           b         n 
         static BigInteger Decrypt(Tuple<BigInteger, BigInteger, BigInteger> cipherText, Tuple<BigInteger, BigInteger, BigInteger, BigInteger> key)
         {
-            var bHalf = key.Item3 * Inverse(TWO, key.Item4);
-            var temp = (cipherText.Item1 + BigInteger.ModPow(bHalf, TWO, key.Item4)) % key.Item4; // (y + b^2/4) mod n 
+            var bHalf = (key.Item3 * Inverse(TWO, key.Item4)) % key.Item4;
+            var temp = (cipherText.Item1 + BigInteger.ModPow(bHalf, TWO, key.Item4)) % key.Item4; 
             var sqrts = BlumSqrt(temp, key.Item1, key.Item2);
+
             BigInteger x = BigInteger.Zero;
             if (sqrts.Item1 % TWO == cipherText.Item2 && JacobiSymbol(sqrts.Item1, key.Item4) == cipherText.Item3) { x = sqrts.Item1; }
+                //Console.WriteLine(sqrts.Item1.ToString("X"));
             if (sqrts.Item2 % TWO == cipherText.Item2 && JacobiSymbol(sqrts.Item2, key.Item4) == cipherText.Item3) { x = sqrts.Item2; }
+                //Console.WriteLine(sqrts.Item2.ToString("X"));
             if (sqrts.Item3 % TWO == cipherText.Item2 && JacobiSymbol(sqrts.Item3, key.Item4) == cipherText.Item3) { x = sqrts.Item3; }
+                //Console.WriteLine(sqrts.Item3.ToString("X"));
             if (sqrts.Item4 % TWO == cipherText.Item2 && JacobiSymbol(sqrts.Item4, key.Item4) == cipherText.Item3) { x = sqrts.Item4; }
-            x -= key.Item3 / TWO;
+            //    Console.WriteLine(sqrts.Item4.ToString("X"));
+            Console.WriteLine(x.ToString("X"));
+            x -= bHalf;
+            //Console.WriteLine(x.ToString("X"));
             return DeformatMessage(x, key.Item4);
 
+        }
+
+
+        static BigInteger Sign(BigInteger m, Tuple<BigInteger, BigInteger, BigInteger, BigInteger> key)
+        {
+            BigInteger x = FormatMessage(m, key.Item4);
+            while (JacobiSymbol(x, key.Item1) != 1 || JacobiSymbol(x, key.Item2) != 1)
+            {
+                r = GeneratePrime(8);
+                x = FormatMessage(m, key.Item4);
+            }
+            var sqrts = BlumSqrt(x, key.Item1, key.Item2);
+            return sqrts.Item1;// лучше возвращать не всегда второй, а случайный из всех четырех
+        }
+
+        static bool Verify(BigInteger s, BigInteger m, BigInteger n)
+           => DeformatMessage(BigInteger.ModPow(s, TWO, n), n) == m;
+
+
+        /* Zero Knoledge Protocol*/
+        
+        static BigInteger ZKPsendY(BigInteger n, BigInteger x)
+            => BigInteger.ModPow(x, FOUR, n);
+
+
+        static BigInteger ZPKrecieveYsendZ(BigInteger p, BigInteger q, BigInteger y)
+        {
+            BigInteger z = 0;
+            var sqrts = BlumSqrt(y, p, q);
+            if (JacobiSymbol(sqrts.Item1, p) == 1 && JacobiSymbol(sqrts.Item1, q) == 1) { z = sqrts.Item1; }
+            if (JacobiSymbol(sqrts.Item2, p) == 1 && JacobiSymbol(sqrts.Item2, q) == 1) { z = sqrts.Item2; }
+            if (JacobiSymbol(sqrts.Item3, p) == 1 && JacobiSymbol(sqrts.Item3, q) == 1) { z = sqrts.Item3; }
+            if (JacobiSymbol(sqrts.Item4, p) == 1 && JacobiSymbol(sqrts.Item4, q) == 1) { z = sqrts.Item4; }
+            return z;
+        }
+
+        static bool ZPKrecieveZ(BigInteger n, BigInteger z, BigInteger x)
+            => BigInteger.ModPow(x, TWO, n) == z;
+
+        /* Zero Knoledge Protocol Attack*/
+
+
+        static BigInteger ZKP_Attack_sendY(BigInteger n, BigInteger t)
+            => BigInteger.ModPow(t, TWO, n);
+
+        static BigInteger ZKP_Attack_getDivider(BigInteger z, BigInteger n, BigInteger t)
+        {
+            if (t == z || t == -z)
+            {
+                Console.WriteLine("Could not find p or q");
+                return BigInteger.Zero;
+            }
+            else
+            {
+                var pq = BigInteger.GreatestCommonDivisor(t + z, n);
+                if (pq == BigInteger.One)        // можно убрать этот if и просто вернуть gcd, если ==, то атака не удалась
+                {
+                    Console.WriteLine("Could not find p or q");
+
+                }
+                return pq;
+            }
         }
 
         static void Main(string[] args)
         {
 
-            // var n = ParseHex("A0B708E4AB23EAC859794C6DE04CA362B0F437F54A6CEFADF6FDF721A015E6D54F883D9BB3F1E1585175B72954131F9FC8680B10189D2C765430C832A1552D65");
-            // var b = ParseHex("3B3A9ECC0BBA6D652CFBD0904F76EA80296186C9A82A884FA55240E1AF74C35C2B8D0E6290C4A87C25CF7FB01D5F7108FDA9645499686A20F749805018A418A6");
+            var rndm = new Random();
+            
+            var keys = GenerateKey(32, 32);// p q b n 
+            Console.WriteLine("p = " + keys.Item1.ToString("X"));
+            Console.WriteLine("q = " + keys.Item2.ToString("X"));
+            Console.WriteLine("b = " + keys.Item3.ToString("X"));
+            Console.WriteLine("n = " + keys.Item4.ToString("X"));
+
+            //var message = ParseHex("761A69515C481F255CDD2FCA2568297A23");
+            //var formated = FormatMessage(message, keys.Item4);
+            //Console.WriteLine("formated :   " + formated.ToString("X"));
+
+            //var cipherText = Encrypt(formated, keys.Item3, keys.Item4);
+            //var dec = Decrypt(cipherText, keys);
+            //Console.WriteLine(dec.ToString("X"));
+
+            while (true)
+            {
+                Console.WriteLine();
+                Console.WriteLine("==================================================");
+                Console.WriteLine();
+
+                Console.WriteLine("What do you want to do?");
+                Console.WriteLine("----------");
+                Console.WriteLine("Enc - Encryption");
+                Console.WriteLine("Dec - Decryption");
+                Console.WriteLine("Sign - Sign");
+                Console.WriteLine("Ver - Verification");
+                Console.WriteLine("ZKP - Zero Knowledge Protol");
+                Console.WriteLine("A - Zero Knowledge Attack");
+                Console.WriteLine("END - End");
+                Console.WriteLine("----------");
+                Console.Write("Your choice: ");
+                string action = Console.ReadLine();
+
+                if (action == "End")
+                {
+                    break;
+                }
+
+                if (action == "Enc")
+                {
+                    Console.WriteLine("----------");
+                    Console.Write("Enter M: ");
+                    string M_hex = Console.ReadLine();
+                    var M = ParseHex(M_hex);
+                    Console.Write("Enter n: ");
+                    string n_hex = Console.ReadLine();
+                    var n = ParseHex(n_hex);
+                    Console.Write("Enter b: ");
+                    string b_hex = Console.ReadLine();
+                    var b = ParseHex(b_hex);
+                    var formated = FormatMessage(M, n);
+                    var C = Encrypt(formated, b, n);
+                    Console.WriteLine("Y = " + C.Item1.ToString("X"));
+                    Console.WriteLine("Parity = " + C.Item2.ToString("X"));
+                    Console.WriteLine("Jacobi = " + C.Item3.ToString("X"));
+                    Console.WriteLine("----------");
+                }
+
+                if (action == "Dec")
+                {
+                    Console.WriteLine("----------");
+
+                    Console.Write("Enter cipherText: ");
+                    string c_hex = Console.ReadLine();
+                    var c = ParseHex(c_hex);
+
+                    Console.Write("Enter parity: ");
+                    string parity_hex = Console.ReadLine();
+                    var parity = ParseHex(parity_hex);
+
+                    Console.Write("Enter jacobi: ");
+                    string jacobi_hex = Console.ReadLine();
+                    var jacobi = ParseHex(jacobi_hex);
+
+                    var cipher = Tuple.Create(c, parity, jacobi);
+
+                    var dec = Decrypt(cipher, keys);
+                    Console.WriteLine("Decrypted: " + dec.ToString("X"));
+                    Console.WriteLine("----------");
+                }
+
+                if (action == "Sign")
+                {
+                    Console.WriteLine("----------");
+                    Console.Write("Enter M: ");
+                    string M_hex = Console.ReadLine();
+                    var M = ParseHex(M_hex);
+                    var s = Sign(M, keys);
+                    Console.WriteLine("S = " + s.ToString("X"));
+                    Console.WriteLine("----------");
+                }
+
+                if (action == "Ver")
+                {
+                    Console.WriteLine("----------");
+                    Console.Write("Enter M: ");
+                    string M_hex = Console.ReadLine();
+                    var M = ParseHex(M_hex);
+
+                    Console.Write("Enter S: ");
+                    string S_hex = Console.ReadLine();
+                    var S = ParseHex(S_hex);
+
+                    Console.Write("Enter n: ");
+                    string n_hex = Console.ReadLine();
+                    var n = ParseHex(n_hex);
+
+                    var ver = Verify(S, M, n);
+                    Console.WriteLine("Verification result :" + ver);
+                    Console.WriteLine("----------");
+                }
+
+                if (action == "ZKP") //  МОЖЕТ ТУТ ПО ДРУГОМУ НАДО  ? ? ? ? ? 
+                {
+                    Console.WriteLine("----------");
 
 
-            var n = ParseHex("A0B708E4AB23EAC859794C6DE04CA362B0F437F54A6CEFADF6FDF721A015E6D54F883D9BB3F1E1585175B72954131F9FC8680B10189D2C765430C832A1552D65");
-            var b = ParseHex("3B3A9ECC0BBA6D652CFBD0904F76EA80296186C9A82A884FA55240E1AF74C35C2B8D0E6290C4A87C25CF7FB01D5F7108FDA9645499686A20F749805018A418A6");
+                    // я не смогу серверу сказать свой n, 
+                    // но при єтом не могу получить его p, q  без атаки
 
-            Console.WriteLine("b =   " + b.ToByteArray().Length);
-            Console.WriteLine("n =   " + n.ToByteArray().Length);
+                    Console.WriteLine("----------");
+                }
 
-            Console.WriteLine("b =   " + b/*.ToString("X")*/);
-            Console.WriteLine("n =   " + n/*.ToString("X")*/);
+                if (action == "A")
+                {
+                    while (true)
+                    {
+                        int tByteLength = rndm.Next(1, 64);
+                        BigInteger t = GeneratePrime(tByteLength); // хотя оно может быть и не простое 
 
-            Console.WriteLine(r.ToByteArray().Length);
+                        Console.Write("Enter n:");
+                        string n_hex = Console.ReadLine();
+                        var n = ParseHex(n_hex);
 
-            var message = "A7A72D350B6E2391676758574847";
-            var formated = FormatMessage(ParseHex(message), n);
-            Console.WriteLine(formated.ToString("X"));
-            Console.WriteLine(formated.ToByteArray().Length);
-            var encrypted = Encrypt(formated, b, n);
+                        var fauxY = ZKP_Attack_sendY(n, t);
+                        Console.WriteLine("y  =  " + fauxY.ToString("X"));
 
-            Console.WriteLine("=========================================");
-            Console.WriteLine("y =   " + encrypted.Item1.ToString("X"));
-            Console.WriteLine("c1 =   " + encrypted.Item2.ToString("X"));
-            Console.WriteLine("c2 =   " + encrypted.Item3.ToString("X"));
+                        Console.Write("Enter z:");
+                        string z_hex = Console.ReadLine();
+                        var z = ParseHex(z_hex);
+
+                        var foundP = ZKP_Attack_getDivider(z, n, t);
+                        if (foundP != BigInteger.One)
+                        {
+                            Console.WriteLine("P  =  " + foundP.ToString("X"));
+                            var foundQ = n / foundP;
+                            Console.WriteLine("Q  =  " + foundQ.ToString("X"));
+
+                            //Console.Write("computed n =" );
+                            //Console.Write((foundP * foundQ).ToString("X"));
+                            //Console.WriteLine(foundQ * foundP == n);
+                            break;
+                        }
+                    }
+
+                }
 
 
-            //var bHalf = (b* Inverse(TWO, n))%n;
-            //var temp = (encrypted.Item1 + BigInteger.ModPow(bHalf, TWO,n)) % n; // (y + b^2/4) mod n 
-            //Console.WriteLine("x =   " + temp.ToString("X"));
+            }
             Console.ReadKey();
         }
 
